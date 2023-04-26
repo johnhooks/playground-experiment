5d3e40639a3ae14624e3039a2f1cd062a093bcd6
John Hooks <bitmachina@outlook.com>
2023-04-26 12:26:53 -0700

    refactor: use blueprint
    publish website

--- src/playground.js
+++ src/playground.js
@@ -4,33 +4,51 @@ import {
   login,
 } from "@wp-playground/client";
 
-import './style.css';
+import "./style.css";
 
 export async function main() {
   const root = document.getElementById("root");
   const iframe = render(root);
+  const response = await fetch(
+    "/playground-experiment/wp-feature-notifications.zip"
+  );
+  const blob = await response.blob();
+  const pluginZipFile = new File([blob], "wp-feature-notifications.zip");
 
   const playground = await startPlaygroundWeb({
     iframe,
     remoteUrl: "https://playground.wordpress.net/remote.html",
+    blueprint: {
+      landingPage: "/wp-admin/",
+      preferredVersions: {
+        php: "8.0",
+        wp: "latest",
+      },
+      steps: [
+        { step: "login", username: "admin", password: "password" },
+        {
+          step: "setSiteOptions",
+          options: {
+            WPLANG: "en",
+            permalink_structure: "/%postname%/",
+            gp_enable_local_translation: 1,
+            gp_enable_inline_translation: 1,
+          },
+        },
+        {
+          step: "updateUserMeta",
+          meta: {
+            show_welcome_panel: "0",
+          },
+          userId: 1,
+        },
+        {
+          step: "installPlugin",
+          pluginZipFile,
+        },
+      ],
+    },
   });
-
-  await playground.isReady();
-
-  await login(playground, { username: "admin", password: "password" });
-
-  const pluginZipFile = await fetchPlugin();
-
-  await installPlugin(playground, { pluginZipFile, activate: true });
-
-  await playground.goTo("/wp-admin");
-}
-
-async function fetchPlugin() {
-  const response = await fetch("/playground-experiment/wp-feature-notifications.zip");
-  const blob = await response.blob();
-  const file = new File([blob], "wp-feature-notifications.zip");
-  return file;
 }
 
 /**
@@ -38,7 +56,7 @@ async function fetchPlugin() {
  * @param {HTMLElement} el
  */
 function render(el) {
-  const iframe = document.createElement('iframe');
+  const iframe = document.createElement("iframe");
   iframe.title = "Playground Viewport";
   iframe.className = "playground__viewport--full-size";
   el.appendChild(iframe);
